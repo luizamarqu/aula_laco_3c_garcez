39ee14da6f5d5d0327b763086830191b0cfb650c
Luiza Marques dos Passos <107187808+luizamarqu@users.noreply.github.com>
2024-04-19 09:38:45 -0300

    Palmeiras

--- main.js
+++ main.js
@@ -4,11 +4,11 @@ function verificarTime() {
 
   while (continuarPerguntando) {
     
-    var respostaTime = prompt("Qual os melhor time?");
+    var respostaTime = prompt("Qual o melhor time?");
 
     
-    if (respostaTime.toLowerCase() === "Athlético PR") {
-      alert("Isso mesmo! O Athlético PR é o melhor time!");
+    if (respostaTime.toLowerCase() === "Palmeiras") {
+      alert("Isso mesmo! O Palmeiras é o melhor time!");
       continuarPerguntando = false; // Sai do loop após a resposta correta
     } else {
       alert("Não é isso! Tente novamente.");
